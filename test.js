9d82441d44a90469e745063afc29922d22bbd6dd
jonschlinkert <jon.schlinkert@sellside.com>
2015-05-13 14:49:13 -0400

    adds test file

---
+++ test.js
@@ -0,0 +1,17 @@
+/*!
+ * grunt-legacy-fail <https://github.com/gruntjs/grunt-legacy-fail>
+ *
+ * Copyright (c) 2015 "Cowboy" Ben Alman.
+ * Licensed under the MIT license.
+ */
+
+'use strict';
+
+/* deps: mocha */
+var assert = require('assert');
+var should = require('should');
+var fail = require('./');
+
+describe('fail', function () {
+
+});
