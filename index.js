8fa26e799b6b4f168fa78f48655341f93f4ba619
jonschlinkert <jon.schlinkert@sellside.com>
2015-05-14 06:12:56 -0400

    metadata:
    - adds "built with grunt" badge
    - related projects
    - updates "Running tests" section on readme with grunt instructions
    - fixes .gitignore pattern
    - updates editor config to ignore all test files

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 /*!
- * grunt-legacy-fail <http://gruntjs.com/grunt-legacy-fail>
+ * grunt <http://gruntjs.com/>
  *
- * Copyright (c) 2015, "Cowboy" Ben Alman.
+ * Copyright (c) 2013-2015 "Cowboy" Ben Alman.
  * Licensed under the MIT license.
  */
 
