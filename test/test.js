fcf8caa8d98c01b7d91bfbc1c62307252e4db619
jonschlinkert <jon.schlinkert@sellside.com>
2015-05-14 06:11:22 -0400

    move test file

---
+++ test/test.js
@@ -0,0 +1,16 @@
+/*!
+ * grunt <http://gruntjs.com/>
+ *
+ * Copyright (c) 2013-2015 "Cowboy" Ben Alman.
+ * Licensed under the MIT license.
+ */
+
+'use strict';
+
+/* deps: mocha */
+var assert = require('assert');
+var fail = require('../');
+
+describe('fail', function () {
+
+});
