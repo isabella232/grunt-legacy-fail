131bed6bde2947d1d00fbfae496fcabe8d130adc
jonschlinkert <jon.schlinkert@sellside.com>
2015-05-14 06:13:15 -0400

    adds gruntfile

---
+++ Gruntfile.js
@@ -0,0 +1,28 @@
+/*!
+ * grunt <http://gruntjs.com/>
+ *
+ * Copyright (c) 2013-2015 "Cowboy" Ben Alman.
+ * Licensed under the MIT license.
+ */
+
+'use strict';
+
+module.exports = function(grunt) {
+
+  // Project configuration.
+  grunt.initConfig({
+    jshint: {
+      all: ['*.js'],
+      options: {
+        reporter: require('jshint-stylish'),
+        jshintrc: '.jshintrc'
+      }
+    }
+  });
+
+  // These plugins provide necessary tasks.
+  grunt.loadNpmTasks('grunt-contrib-jshint');
+
+  // Default task.
+  grunt.registerTask('default', ['jshint']);
+};
